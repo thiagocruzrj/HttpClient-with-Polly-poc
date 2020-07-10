f5a814eba8026de2d1b32fa2d234e8c2167ca019
Thiago Cruz <thagocruz@gmail.com>
2020-07-10 15:44:26 -0300

    Add concrete implementation

---
+++ src/HttpClientFactoryPoc/HttpClientFactoryProject/Services/TodoService.cs
@@ -0,0 +1,13 @@
+﻿using HttpClientFactoryProject.Models;
+using System.Threading.Tasks;
+
+namespace HttpClientFactoryProject.Services
+{
+    public class TodoService : ITodoService
+    {
+        public Task<TodoModel> GetTodo(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
