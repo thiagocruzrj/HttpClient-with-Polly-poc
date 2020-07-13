f6009a0d176d7343c98dd3690fa0afea9e9ae72e
Thiago Cruz <thagocruz@gmail.com>
2020-07-13 12:28:30 -0300

    Create TodoController.cs

---
+++ src/HttpClientFactoryPoc/HttpClientFactoryProject/Controller/TodoController.cs
@@ -0,0 +1,19 @@
+﻿using HttpClientFactoryProject.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace HttpClientFactoryProject.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoController : ControllerBase
+    {
+        private readonly ITodoService _todoService;
+
+        public TodoController(ITodoService todoService)
+        {
+            _todoService = todoService;
+        }
+    }
+}
